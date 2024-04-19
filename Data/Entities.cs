7e2d7f58d88cca3c32f21a859bd7906c6298ab2c
ritchyboy <ritchyboy1@hotmail.com>
2024-04-19 17:55:03 -0400

    data add

---
+++ Data/Entities.cs
@@ -0,0 +1,22 @@
+﻿using Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class Entities:DbContext
+    {
+        public DbSet<Flight> Flights => Set<Flight>();
+        public Entities(DbContextOptions option):base(option) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Flight>().HasKey(f => f.Id);
+            modelBuilder.Entity<Flight>().OwnsMany(f => f.BookingList);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
