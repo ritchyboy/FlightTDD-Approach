2f267850437fe99141a2c7195b81ae24f510c255
ritchyboy <ritchyboy1@hotmail.com>
2024-04-19 17:53:40 -0400

    add domains

---
+++ Domains/Flight.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domains
+{
+    public class Flight
+    {
+        private List<Booking> bookingList = new();
+        public IEnumerable<Booking> BookingList => bookingList;
+
+        public int RemainingNumberOfSeat { get; set; }
+
+        public Guid Id { get; }
+
+        [Obsolete("Needed for EF")]
+
+        Flight() { }
+
+            public Flight(int seatCapacity)
+            {
+              RemainingNumberOfSeat = seatCapacity;
+            }
+
+    public object? Book(string passengerEmail, int numberOfSeats)
+    {
+        if (numberOfSeats > this.RemainingNumberOfSeat)
+            return new OverBookingError();
+
+        RemainingNumberOfSeat -= numberOfSeats;
+
+        bookingList.Add(new Booking(passengerEmail, numberOfSeats));
+        return null;
+    }
+
+    public object? CancelBooking(string passengerEmail, int numberOfSeats)
+    {
+        if (!bookingList.Any(booking => booking.Email == passengerEmail))
+            return new BookingNotFoundError();
+
+        RemainingNumberOfSeat += numberOfSeats;
+        return null;
+    }
+  }   
+}
